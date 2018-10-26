16e296332b6c1b577bc7b7d5ffa8d349c325fc05
Marcos Ivan Robles Hernandez <marcos92robles@gmail.com>
2018-10-26 16:13:20 -0500

    Connection string correcta. Get, Get ID y Delete correctos.
    POST (Crear) y PUT (Modificar) dan error

--- Workouts/Controllers/ValuesController.cs
+++ Workouts/Controllers/ValuesController.cs
@@ -13,14 +13,14 @@ namespace Workouts.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Nombre", "Marcos" };
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            return "Marcos";
         }
 
         // POST api/values
