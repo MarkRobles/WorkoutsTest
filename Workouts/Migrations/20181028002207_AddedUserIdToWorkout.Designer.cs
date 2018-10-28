fee07bdb456b767d499f4023f204c1749b21cf22
Marcos Ivan Robles Hernandez <marcos92robles@gmail.com>
2018-10-27 19:25:04 -0500

    Agregar migracion a base de datos

--- Workouts/Migrations/20181028002207_AddedUserIdToWorkout.Designer.cs
+++ Workouts/Migrations/20181028002207_AddedUserIdToWorkout.Designer.cs
@@ -11,9 +11,10 @@ using Workouts.Models;
 namespace Workouts.Migrations
 {
     [DbContext(typeof(WorkoutsContext))]
-    partial class WorkoutsContextModelSnapshot : ModelSnapshot
+    [Migration("20181028002207_AddedUserIdToWorkout")]
+    partial class AddedUserIdToWorkout
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
